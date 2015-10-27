b80210ce51f0316f6b93231faa29ef2494567b4d
lijingjing123 <846772490@qq.com>
2015-10-27 11:50:10 +0800

    11:49

--- src/Js/index.js
+++ src/Js/index.js
@@ -73,4 +73,70 @@ $(function() {
       var index =$(this).index();
        tab(index);
         })
+
+   // 路径配置
+    require.config({
+        paths: {
+            echarts: '../jQuery/Echarts/dist'
+        }
+    });
+    // 使用
+    require(
+        [
+            'echarts',
+            'echarts/chart/line' // 使用柱状图就加载bar模块，按需加载
+        ],
+        function(ec) {
+            // 基于准备好的dom，初始化echarts图表
+            var myChart = ec.init(document.getElementById("main"));
+            var option = {
+                title: {
+                    //text: '未来一周气温变化',
+                    //subtext: '纯属虚构'
+                },
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                    data: ['']
+                },
+                
+                calculable: true,
+                xAxis: [{
+                    type: 'category',
+                    boundaryGap: false,
+                    data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+                }],
+                yAxis: [{
+                    type: 'value',
+                    xisLabel: {
+                        formatter: '{value} °C'
+                    }
+                }],
+                series: [{
+                    name: '最高气温',
+                    type: 'line',
+                    data: [11, 11, 15, 13, 12, 13, 10],
+                    markPoint: {
+                        data: [{
+                            type: 'max',
+                            name: '最大值'
+                        }, {
+                            type: 'min',
+                            name: '最小值'
+                        }]
+                    },
+                    markLine: {
+                        data: [{
+                            type: 'average',
+                            name: '平均值'
+                        }]
+                    }
+                }]
+            };
+            // 为echarts对象加载数据 
+            myChart.setOption(option);
+        }
+    )
+
 })
